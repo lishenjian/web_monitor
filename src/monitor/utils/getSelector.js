6b63649d7cf281036733e35384e82a17df6f8e7f
lishenjian <lishenjian@tripwise.cn>
2021-05-29 10:57:55 +0800

    扩展日志数据+tracker

---
+++ src/monitor/utils/getSelector.js
@@ -0,0 +1,22 @@
+function getSelectors(path) {
+
+    return path.reverse().filter(element=>{
+        return element !== document && element !== window;
+    }).map(element=>{
+        let selector = '';
+        if(element.id) {
+            return `${element.nodeName.toLowerCase()}#${element.id}`
+        }else if(element.className && typeof element.className === 'string') {
+            return `${element.nodeName.toLowerCase()}.${element.className}`
+        }else {
+            selector = element.nodeName.toLowerCase()
+        }
+        return selector;
+    }).join(' ')
+}
+
+export default function (path) {
+    if(Array.isArray(path)) {
+        return getSelectors(path);
+    }
+}
---
+++ src/monitor/utils/sendLog.js
@@ -0,0 +1,26 @@
+//浏览器检测
+let userAgent = require('user-agent');
+console.log(userAgent);
+// 获取浏览器及用户信息
+function getExtraData() {
+    return {
+        title: document.title,
+        url: location.url,
+        timestamp: Date.now(),
+        userAgent: userAgent.parse(navigator.userAgent),         //用户浏览器类型
+    }
+}
+
+// 发送日志对象
+class SendTracker {
+    constructor() {
+        //设置 url + ip:port
+    }
+
+    send(data = {}) {
+        let extraData = getExtraData();
+        let sendLog = { ...extraData, ...data};
+        console.log(sendLog);
+    }
+}
+export default new SendTracker();
